325358e75aeb398e2d5834db2543b55cfa84507e
Baye LAM <bayesabarane@gmail.com>
2023-08-25 16:22:08 +0000

    Adding Search Feature with more fields

--- product/filters.py
+++ product/filters.py
@@ -4,6 +4,10 @@ from .models import Product
 
 class ProductsFilter(filters.FilterSet):
 
+    keyword = filters.CharFilter(field_name="name", lookup_expr="icontains")
+    min_price = filters.NumberFilter(field_name="price" or 0, lookup_expr="gte")
+    max_price = filters.NumberFilter(field_name="price" or 10000, lookup_expr="lte")
+
     class Meta:
         model = Product
-        fields = ('category', 'brand')
+        fields = ('keyword', 'category', 'brand', 'min_price', 'max_price')
